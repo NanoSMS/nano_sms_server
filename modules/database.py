5628b0fd3992cf4be5196f7fdee979d350d24ef7
TertiusN <35367281+TertiusN@users.noreply.github.com>
2018-07-24 22:15:01 +0200

    Updated Database
    Now supports recovery words

--- modules/database.py
+++ modules/database.py
@@ -1,7 +1,10 @@
+import os
+
 from peewee import *
 
-
-db = SqliteDatabase("../users.db")  # Database system here
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../database.db')
+db = SqliteDatabase(filename)  # Database system here
 
 
 class BaseModel(Model):
@@ -21,9 +24,12 @@ class User(BaseModel):
     trust_address = CharField(null=True)
     trust_phonenumber = CharField(null=True)
 
+    rec_word = CharField()
+
 
 tables = [User]
 
 
 if __name__ == "__main__":
     db.create_tables(tables)
+
