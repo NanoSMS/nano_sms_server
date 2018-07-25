166609a7749fc195157bda70c787fbaae93e64a6
TertiusN <35367281+TertiusN@users.noreply.github.com>
2018-07-25 19:54:59 +0200

    Update loadcards.py

--- loadcards.py
+++ loadcards.py
@@ -5,10 +5,10 @@ db.connect()
 
 #Placeholders, for example only
 load_cards = [
-	{'cardcode': 'AAA123', 'cardvalue': 100, 'cardsn': 'NANO-S-TEST'},
-    {'cardcode': 'BBB123', 'cardvalue': 1, 'cardsn': 'NANO-S-001'},
-    {'cardcode': 'CCC123', 'cardvalue': 2, 'cardsn': 'NANO-S-002'},
-    {'cardcode': 'DDD123', 'cardvalue': 3, 'cardsn': 'NANO-S-003'},
+	{'cardcode': 'AAA123', 'cardvalue': 100, 'cardsn': 'NANO-S-TEST', 'claimed': False},
+    {'cardcode': 'BBB123', 'cardvalue': 1, 'cardsn': 'NANO-S-001', 'claimed': False},
+    {'cardcode': 'CCC123', 'cardvalue': 2, 'cardsn': 'NANO-S-002', 'claimed': False},
+    {'cardcode': 'DDD123', 'cardvalue': 3, 'cardsn': 'NANO-S-003', 'claimed': False}
     # ...
 ]
 
